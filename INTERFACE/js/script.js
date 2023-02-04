dba65ba5a66c510d2e737c6f3e8f71a777964fed
Nathália Santos <63311872+NathSantos@users.noreply.github.com>
2023-02-04 18:41:25 -0400

    Update script.js

--- INTERFACE/js/script.js
+++ INTERFACE/js/script.js
@@ -3,7 +3,6 @@ var titulo = document.querySelector("h1");
 var srchBox = document.querySelector(".search-box");
 var srchTxt = document.querySelector(".search-text");
 var srchBtn = document.querySelector(".lupa-btn");
-var srchBtnHover = document.querySelector(".search-box:hover.dark");
 var labelBtn = document.querySelector(".label");            // AJEITAR
 var labelBallBtn = document.querySelector(".label .ball");   // AJEITAR
 
@@ -26,7 +25,6 @@ chk.addEventListener('change', function() {
     srchBox.classList.toggle('dark');
     srchTxt.classList.toggle('dark');
     srchBtn.classList.toggle('dark');
-    srchBtnHover.classList.toggle('dark');
     labelBtn.classList.toggle('dark');
     labelBallBtn.classList.toggle('dark');
 });
